4d4441737e5e0438a81b2b26f3c83ac23dcfa473
Seong Ui-je <uije91@gmail.com>
2024-04-30 02:29:44 +0900

    feat: jwt 기능 구현

--- src/main/java/com/unity/goods/global/jwt/UserDetailsImpl.java
+++ src/main/java/com/unity/goods/global/jwt/UserDetailsImpl.java
@@ -1,5 +1,51 @@
 package com.unity.goods.global.jwt;
 
-public class CustomUserDetails {
+import com.unity.goods.domain.member.entity.Member;
+import java.util.ArrayList;
+import java.util.Collection;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+  private final Member member;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    Collection<GrantedAuthority> authorities = new ArrayList<>();
+    authorities.add(() -> member.getRole().getKey());
+    return authorities;
+  }
+
+  @Override
+  public String getUsername() {
+    return member.getEmail();
+  }
+
+  @Override
+  public String getPassword() {
+    return member.getPassword();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() { // 계정의 만료 여부
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() { // 계정의 잠김 여부
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() { // 비밀번호 만료 여부
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() { // 계정의 활성화 여부
+    return true;
+  }
 }
