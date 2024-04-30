087637e1eeb7e188835b6a1a68ccfd6340042a40
seowonn <sonseowon5@gmail.com>
2024-04-30 23:37:47 +0900

    Feat : 인증 번호를 담은 email 보내는 기능 추가 - 현재 smtp gmail만 보내지게 되어 있음 - 간단한 인증 메일이라 MimeMessage 대신 SimpleMailMessage 사용

--- src/main/java/com/unity/goods/domain/email/service/EmailService.java
+++ src/main/java/com/unity/goods/domain/email/service/EmailService.java
@@ -1,10 +1,16 @@
 package com.unity.goods.domain.email.service;
 
+import static com.unity.goods.global.exception.ErrorCode.EMAIL_SEND_ERROR;
+
 import com.unity.goods.domain.email.dto.EmailVerificationDto.EmailVerificationRequest;
+import com.unity.goods.domain.email.exception.EmailException;
+import com.unity.goods.domain.email.type.EmailSubjects;
 import com.unity.goods.global.service.RedisService;
 import java.security.SecureRandom;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,17 +19,31 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
   private final static Long VERIFICATION_EXPIRED_AT = 60 * 3 * 1000L; // 3분
+  private final static String SENDER = "Goods";
+
   private final RedisService redisService;
+  private final MailSender mailSender;
 
   public void sendVerificationEmail(
       EmailVerificationRequest emailVerificationRequest) {
 
     String verificationNumber = createVerificationNumber();
 
-    redisService.setDataExpire(emailVerificationRequest.getEmail(),
-        verificationNumber,
-        VERIFICATION_EXPIRED_AT);
-    log.info("[sendVerificationEmail] : 생성된 인증 번호 {} ", verificationNumber);
+    SimpleMailMessage verificationEmail = createVerificationEmail(
+        emailVerificationRequest.getEmail(), verificationNumber);
+
+    try {
+      mailSender.send(verificationEmail);
+      redisService.setDataExpire(emailVerificationRequest.getEmail(), verificationNumber,
+          VERIFICATION_EXPIRED_AT);
+      log.info("[sendVerificationEmail] : 이메일 전송 완료. 인증 번호 redis 저장");
+
+    } catch (RuntimeException e) {
+      log.debug("[sendVerificationEmail] : 이메일 전송 과정 중 에러 발생");
+      throw new EmailException(EMAIL_SEND_ERROR);
+
+    }
+
   }
 
   public String createVerificationNumber() {
@@ -33,6 +53,21 @@ public class EmailService {
     log.info("[createVerificationNumber] : 인증 번호 생성 완료");
 
     return String.valueOf(randomNumber);
+  }
+
+  public SimpleMailMessage createVerificationEmail(String emailAddress, String verificationNumber) {
+
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setFrom(SENDER);
+    message.setTo(emailAddress);
+    message.setSubject(EmailSubjects.SEND_VERIFICATION_CODE.getTitle());
+    message.setText(
+        "안녕하세요. 중고거래 마켓 " + SENDER + "입니다.\n\n"
+            + "인증 번호는 [" + verificationNumber + "] 입니다.\n\n"
+            + "인증 번호를 입력하고 인증 완료 버튼을 눌러주세요.");
+
+    log.info("[createVerificationEmail] : 인증 이메일 생성 완료");
+    return message;
   }
 
 }
--- src/main/java/com/unity/goods/global/exception/ErrorCode.java
+++ src/main/java/com/unity/goods/global/exception/ErrorCode.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 public enum ErrorCode {
   // System Error
   INTERNAL_SERVER_ERROR(500, "내부 서버 오류가 발생했습니다."),
-  BAD_REQUEST_VALID_ERROR(400, "유효성 검사에 실패했습니다.");
+  BAD_REQUEST_VALID_ERROR(400, "유효성 검사에 실패했습니다."),
+
+  // Email Error
+  EMAIL_SEND_ERROR(500, "이메일 전송 과정 중 에러가 발생하였습니다.");
 
   private final int status;
   private final String message;
