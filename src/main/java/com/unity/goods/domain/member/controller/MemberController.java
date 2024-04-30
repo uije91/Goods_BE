fa8ab0af174d51d20194c5909b51cbd160bd78b6
seowonn <sonseowon5@gmail.com>
2024-04-30 15:30:07 +0900

    Feat : member 컨트롤러 입출력 설정 - SignUp Request & Response 추가

--- src/main/java/com/unity/goods/domain/member/controller/MemberController.java
+++ src/main/java/com/unity/goods/domain/member/controller/MemberController.java
@@ -1,5 +1,27 @@
 package com.unity.goods.domain.member.controller;
 
+import com.unity.goods.domain.member.dto.SignUpRequest;
+import com.unity.goods.domain.member.dto.SignUpResponse;
+import com.unity.goods.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
 public class MemberController {
+
+  private final MemberService memberService;
+
+  @PostMapping("/signup")
+  public ResponseEntity<?> signUpMember(
+      @RequestBody SignUpRequest signUpRequest){
+    SignUpResponse signUpResponse = memberService.signUpMember(signUpRequest);
+    return ResponseEntity.ok(signUpResponse);
+  }
 
 }
