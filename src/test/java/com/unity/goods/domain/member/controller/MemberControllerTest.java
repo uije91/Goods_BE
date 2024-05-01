4d5826132888c5e0f0774270d88c4b38244bb1b0
heechan <shgmlcks1004@naver.com>
2024-05-02 02:29:41 +0900

    Test: 비밀번호 변경 기능 테스트

---
+++ src/test/java/com/unity/goods/domain/member/controller/MemberControllerTest.java
@@ -0,0 +1,48 @@
+package com.unity.goods.domain.member.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.unity.goods.domain.member.dto.ChangePasswordDto.ChangePasswordRequest;
+import com.unity.goods.domain.member.entity.Member;
+import com.unity.goods.domain.member.service.MemberService;
+import com.unity.goods.global.security.WithCustomMockUser;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(MemberController.class)
+class MemberControllerTest {
+
+  @MockBean
+  MemberService memberService;
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  @WithCustomMockUser
+  @DisplayName("비밀번호 변경 테스트")
+  void changePassword() throws Exception {
+
+    doNothing().when(memberService)
+        .changePassword(any(ChangePasswordRequest.class), any(Member.class));
+
+    mockMvc.perform(
+            put("/api/member/change")
+                .with(csrf())
+        )
+        .andExpect(status().isOk())
+        .andDo(print());
+
+  }
+
+
+}
---
+++ src/test/java/com/unity/goods/domain/member/service/MemberServiceTest.java
@@ -0,0 +1,79 @@
+package com.unity.goods.domain.member.service;
+
+import com.unity.goods.domain.member.dto.ChangePasswordDto.ChangePasswordRequest;
+import com.unity.goods.domain.member.entity.Member;
+import com.unity.goods.domain.member.repository.MemberRepository;
+import com.unity.goods.domain.member.type.Role;
+import com.unity.goods.domain.member.type.SocialType;
+import com.unity.goods.domain.member.type.Status;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class MemberServiceTest {
+
+  @Autowired
+  MemberService memberService;
+
+  @Autowired
+  MemberRepository memberRepository;
+
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
+  @BeforeEach
+  public void setMember() {
+    Member member = Member.builder()
+        .nickname("test")
+        .email("test@naver.com")
+        .password("test1234")
+        .star(2.0)
+        .role(Role.USER)
+        .status(Status.INACTIVE)
+        .tradePassword("1234")
+        .socialType(SocialType.SERVER)
+        .build();
+
+    memberRepository.save(member);
+  }
+
+  @Test
+  @Transactional
+  @DisplayName("비밀번호 변경 테스트")
+  void changePassword() {
+    //given
+    Member member = Member.builder()
+        .nickname("test")
+        .email("test@naver.com")
+        .password("test1234")
+        .star(2.0)
+        .role(Role.USER)
+        .status(Status.INACTIVE)
+        .tradePassword("1234")
+        .socialType(SocialType.SERVER)
+        .build();
+
+    ChangePasswordRequest changePasswordRequest = ChangePasswordRequest.builder()
+        .password("new1234")
+        .build();
+
+    //when
+    memberService.changePassword(changePasswordRequest, member);
+    Member changePasswordMember = memberRepository.findByEmail(member.getEmail()).get();
+
+    boolean validateChangePassword = passwordEncoder.matches(changePasswordRequest.getPassword(),
+        changePasswordMember.getPassword());
+
+    //then
+    Assertions.assertTrue(validateChangePassword);
+
+
+  }
+
+}
