a9567fd7780a9fd406ad8fcaf1abf8ffa82bbd00
seowonn <sonseowon5@gmail.com>
2024-05-02 16:54:28 +0900

    Test : 회원 탈퇴 서비스 테스트

--- src/test/java/com/unity/goods/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/unity/goods/domain/member/service/MemberServiceTest.java
@@ -1,12 +1,17 @@
 package com.unity.goods.domain.member.service;
 
+import static com.unity.goods.domain.member.type.Status.ACTIVE;
+import static com.unity.goods.domain.member.type.Status.INACTIVE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.unity.goods.domain.member.dto.SignUpDto.SignUpRequest;
 import com.unity.goods.domain.member.entity.Member;
+import com.unity.goods.domain.member.exception.MemberException;
 import com.unity.goods.domain.member.repository.MemberRepository;
+import com.unity.goods.global.exception.ErrorCode;
 import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -47,6 +52,36 @@ class MemberServiceTest {
       assertTrue(passwordEncoder.matches("123456", member.getTradePassword()));
     }
 
+  }
+
+  @Test
+  @DisplayName("회원 탈퇴 상태 변경")
+  public void resignTest() {
+
+    String originalPw = "12345678";
+    String encode = passwordEncoder.encode(originalPw);
+    // given
+    Member member = Member.builder()
+        .email("fortestseowon@gmail.com")
+        .password(encode)
+        .nickname("nickName")
+        .status(ACTIVE)
+        .tradePassword("123456")
+        .build();
+
+    memberRepository.save(member);
+
+    // when
+//    memberService.resign(member, ResignRequest.builder().password(originalPw).build());
+
+    // then
+    Optional<Member> byEmail = memberRepository.findByEmail(member.getEmail());
+    if(byEmail.isEmpty()){
+      throw new MemberException(ErrorCode.USER_NOT_FOUND);
+    }
+
+    Member savedMember = byEmail.get();
+    assertEquals(INACTIVE, savedMember.getStatus());
   }
 
 
