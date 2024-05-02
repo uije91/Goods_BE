8dc907cca6e5151569ed48195c2a1c4fcf1c9552
heechan <shgmlcks1004@naver.com>
2024-05-03 02:10:10 +0900

    Test: test conflict 해결

--- src/test/java/com/unity/goods/domain/member/controller/MemberControllerTest.java
+++ src/test/java/com/unity/goods/domain/member/controller/MemberControllerTest.java
@@ -1,41 +1,65 @@
 package com.unity.goods.domain.member.controller;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.unity.goods.domain.member.dto.ChangePasswordDto.ChangePasswordRequest;
 import com.unity.goods.domain.member.dto.FindPasswordDto.FindPasswordRequest;
-import com.unity.goods.domain.member.entity.Member;
+import com.unity.goods.domain.member.dto.LoginDto.LoginRequest;
 import com.unity.goods.domain.member.service.MemberService;
 import com.unity.goods.global.jwt.UserDetailsImpl;
 import com.unity.goods.global.security.WithCustomMockUser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(MemberController.class)
 class MemberControllerTest {
 
+  @Autowired
+  MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
   @MockBean
   MemberService memberService;
 
-  @Autowired
-  private MockMvc mockMvc;
+
+  @Test
+  @DisplayName("로그인 성공")
+  @WithMockUser
+  void login_success() throws Exception {
+    String email = "test@test.com";
+    String password = "!q2w3e4r";
+
+    LoginRequest login = LoginRequest.builder().email(email).password(password).build();
+
+    mockMvc.perform(post("/api/member/login")
+            .with(csrf())
+            .content(objectMapper.writeValueAsString(login))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andDo(print());
+  }
 
   @Test
   @WithCustomMockUser
   @DisplayName("비밀번호 변경 테스트")
   void changePassword() throws Exception {
 
-    doNothing().when(memberService)
+    Mockito.doNothing().when(memberService)
         .changePassword(any(ChangePasswordRequest.class), any(UserDetailsImpl.class));
 
     mockMvc.perform(
@@ -44,7 +68,6 @@ class MemberControllerTest {
         )
         .andExpect(status().isOk())
         .andDo(print());
-
   }
 
   @Test
@@ -52,7 +75,7 @@ class MemberControllerTest {
   @DisplayName("비밀번호 찾기 테스트")
   void findPassword() throws Exception {
 
-    doNothing().when(memberService)
+    Mockito.doNothing().when(memberService)
         .findPassword(any(FindPasswordRequest.class));
 
     mockMvc.perform(
@@ -61,8 +84,5 @@ class MemberControllerTest {
         )
         .andExpect(status().isOk())
         .andDo(print());
-
   }
-
-
 }
--- src/test/java/com/unity/goods/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/unity/goods/domain/member/service/MemberServiceTest.java
@@ -5,20 +5,31 @@ import static com.unity.goods.domain.member.type.Status.RESIGN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.unity.goods.domain.member.dto.ChangePasswordDto.ChangePasswordRequest;
+import com.unity.goods.domain.member.dto.FindPasswordDto.FindPasswordRequest;
 import com.unity.goods.domain.member.dto.ResignDto.ResignRequest;
 import com.unity.goods.domain.member.dto.SignUpDto.SignUpRequest;
 import com.unity.goods.domain.member.entity.Member;
 import com.unity.goods.domain.member.exception.MemberException;
 import com.unity.goods.domain.member.repository.MemberRepository;
+import com.unity.goods.domain.member.type.Role;
+import com.unity.goods.domain.member.type.SocialType;
+import com.unity.goods.domain.member.type.Status;
 import com.unity.goods.global.exception.ErrorCode;
+import com.unity.goods.global.jwt.UserDetailsImpl;
 import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class MemberServiceTest {
 
   @Autowired
@@ -29,6 +40,22 @@ class MemberServiceTest {
 
   @Autowired
   private PasswordEncoder passwordEncoder;
+
+  @BeforeEach
+  public void setMember() {
+    Member member = Member.builder()
+        .nickname("test")
+        .email("test@naver.com")
+        .password("test1234")
+        .star(2.0)
+        .role(Role.USER)
+        .status(Status.INACTIVE)
+        .tradePassword("1234")
+        .socialType(SocialType.SERVER)
+        .build();
+
+    memberRepository.save(member);
+  }
 
   @Test
   public void signUpTestWithNoImage() {
@@ -72,8 +99,10 @@ class MemberServiceTest {
 
     memberRepository.save(member);
 
+    UserDetailsImpl userDetails = new UserDetailsImpl(member);
+
     // when
-    memberService.resign(member.getEmail(), ResignRequest.builder().password(originalPw).build());
+    memberService.resign(member.getEmail(), userDetails, ResignRequest.builder().password(originalPw).build());
 
     // then
     Optional<Member> byEmail = memberRepository.findByEmail(member.getEmail());
@@ -85,5 +114,59 @@ class MemberServiceTest {
     assertEquals(RESIGN, savedMember.getStatus());
   }
 
+  @Test
+  @DisplayName("비밀번호 변경 테스트")
+  void changePassword() {
+    //given
+    Member member = Member.builder()
+        .nickname("test")
+        .email("test@naver.com")
+        .password("test12345")
+        .star(2.0)
+        .role(Role.USER)
+        .status(Status.INACTIVE)
+        .tradePassword("1234")
+        .socialType(SocialType.SERVER)
+        .build();
+
+    ChangePasswordRequest changePasswordRequest = ChangePasswordRequest.builder()
+        .password("new1234")
+        .build();
+
+    UserDetailsImpl userDetails = new UserDetailsImpl(member);
+
+    //when
+    memberService.changePassword(changePasswordRequest, userDetails);
+    Member changePasswordMember = memberRepository.findByEmail(member.getEmail()).get();
+
+    boolean validateChangePassword = passwordEncoder.matches(changePasswordRequest.getPassword(),
+        changePasswordMember.getPassword());
+
+    //then
+    Assertions.assertTrue(validateChangePassword);
+
+
+  }
+
+  @Test
+  @DisplayName("비밀번호 찾기 이메일 생성 테스트")
+  void findPasswordEmailTest() {
+    //given
+    FindPasswordRequest findPasswordRequest = FindPasswordRequest.builder()
+        .email("test@naver.com")
+        .build();
+
+    String tempPassword = "1a2B3%571!";
+
+    //when
+    SimpleMailMessage findPasswordEmail
+        = memberService.createFindPasswordEmail(findPasswordRequest.getEmail(), tempPassword);
+
+    //then
+    Assertions.assertEquals(findPasswordEmail.getText(),
+        "안녕하세요. 중고거래 마켓 " + "Goods" + "입니다."
+            + "\n\n" + "임시 비밀번호는 [" + "1a2B3%571!" + "] 입니다.");
+
+  }
 
 }
