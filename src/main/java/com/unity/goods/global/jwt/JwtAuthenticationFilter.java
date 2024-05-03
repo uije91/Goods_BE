30fa40774e3ec589392e01045e8b9b712efc3e04
seowonn <sonseowon5@gmail.com>
2024-05-04 00:46:35 +0900

    Refactor : log 수정 - [클래스 명]으로 수정, 클래스에 담긴 메서드가 많을 경우 [클래스명][메서드명]으로 수정 - 의미 있는 정보를 담기 위해 (민감정보제외) log에 표시되게 설정 - 에러 발생은 log.error로 수정 - log 많이 넣으면 서버에 부담 가는 것 앎. 꼭 필요한 정보만 log 추가

--- src/main/java/com/unity/goods/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/unity/goods/global/jwt/JwtAuthenticationFilter.java
@@ -6,13 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
