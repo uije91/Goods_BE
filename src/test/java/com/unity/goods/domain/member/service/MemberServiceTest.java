b231867f9e2a7a4a5998391b6fdc7ac3cabfec83
seowonn <sonseowon5@gmail.com>
2024-05-04 19:05:54 +0900

    Refactor : 테스트 코드 수정 - 통합 테스트 -> 단위 테스트로 수정 - Mockito Extension 사용 - given으로 테스트 영영 외 환경 설정 - when으로 service 메서드 실행 - then으로 예상 기댓값 비교

--- src/test/java/com/unity/goods/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/unity/goods/domain/member/service/MemberServiceTest.java
@@ -2,8 +2,9 @@ package com.unity.goods.domain.member.service;
 
 import static com.unity.goods.domain.member.type.Status.ACTIVE;
 import static com.unity.goods.domain.member.type.Status.RESIGN;
+import static com.unity.goods.global.exception.ErrorCode.USER_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
 
 import com.unity.goods.domain.member.dto.FindPasswordDto.FindPasswordRequest;
 import com.unity.goods.domain.member.dto.ResignDto.ResignRequest;
@@ -14,31 +15,37 @@ import com.unity.goods.domain.member.repository.MemberRepository;
 import com.unity.goods.domain.member.type.Role;
 import com.unity.goods.domain.member.type.SocialType;
 import com.unity.goods.domain.member.type.Status;
-import com.unity.goods.global.exception.ErrorCode;
+import com.unity.goods.global.jwt.JwtTokenProvider;
 import com.unity.goods.global.jwt.UserDetailsImpl;
+import com.unity.goods.infra.service.RedisService;
 import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 @Transactional
 class MemberServiceTest {
 
-  @Autowired
+  @InjectMocks
   private MemberService memberService;
 
-  @Autowired
+  @Mock
   private MemberRepository memberRepository;
-
-  @Autowired
+  @Mock
   private PasswordEncoder passwordEncoder;
+  @Mock
+  private RedisService redisService;
+  @Mock
+  private JwtTokenProvider jwtTokenProvider;
 
   @BeforeEach
   public void setMember() {
@@ -57,61 +64,87 @@ class MemberServiceTest {
   }
 
   @Test
+  @DisplayName("회원 가입 - 이미지 X 경우(s3 업로드 고려 X) 테스트")
   public void signUpTestWithNoImage() {
-    // given
-    SignUpRequest signUpRequest = SignUpRequest.builder()
-        .email("fortestseowon@gmail.com")
-        .password("12345678")
-        .chkPassword("12345678")
-        .nickName("nickName")
-        .tradePassword("123456")
+
+    String fakeMemberEmail = "fortestseowon@gmail2.com";
+    String encodedPw = "12345678", encodedTradePw = "123456";
+
+    Member member = Member.builder()
+        .email(fakeMemberEmail)
+        .password(encodedPw)
+        .nickname("nickName")
+        .status(ACTIVE)
+        .tradePassword(encodedTradePw)
         .build();
 
+    SignUpRequest mockSignUpRequest = SignUpRequest.builder()
+        .email(fakeMemberEmail)
+        .password(encodedPw)
+        .chkPassword(encodedPw)
+        .nickName("nickName")
+        .tradePassword(encodedTradePw)
+        .build();
+
+    // given
+    given(memberRepository.existsByEmail(mockSignUpRequest.getEmail())).willReturn(false);
+    given(memberRepository.existsByNickname(mockSignUpRequest.getNickName())).willReturn(false);
+    given(passwordEncoder.encode(mockSignUpRequest.getPassword())).willReturn(encodedPw);
+    given(passwordEncoder.encode(mockSignUpRequest.getTradePassword())).willReturn(encodedTradePw);
+
+    given(memberRepository.findByEmail(mockSignUpRequest.getEmail()))
+        .willReturn(Optional.of(member));
+
     // when
-    memberService.signUp(signUpRequest);
+    memberService.signUp(mockSignUpRequest);
 
     // then
-    Optional<Member> byEmail = memberRepository.findByEmail("fortestseowon@gmail.com");
-    if (byEmail.isPresent()) {
-      Member member = byEmail.get();
-      assertEquals(signUpRequest.getNickName(), member.getNickname());
-      assertTrue(passwordEncoder.matches("12345678", member.getPassword()));
-      assertTrue(passwordEncoder.matches("123456", member.getTradePassword()));
-    }
+    Member signUpMember = memberRepository.findByEmail(fakeMemberEmail)
+        .orElseThrow(() -> new MemberException(USER_NOT_FOUND));
 
+    assertEquals(mockSignUpRequest.getNickName(), signUpMember.getNickname());
+    assertEquals(ACTIVE, signUpMember.getStatus());
+    assertEquals(encodedPw, signUpMember.getPassword());
+    assertEquals(encodedTradePw, signUpMember.getTradePassword());
   }
 
   @Test
   @DisplayName("회원 탈퇴 상태 변경")
   public void resignTest() {
 
-    String originalPw = "12345678";
-    String encode = passwordEncoder.encode(originalPw);
+    String encodedPw = "12345678";
     // given
+    String fakeMemberEmail = "fortestseowon@gmail2.com";
     Member member = Member.builder()
-        .email("fortestseowon@gmail2.com")
-        .password(encode)
+        .id(1L)
+        .email(fakeMemberEmail)
+        .password(encodedPw)
         .nickname("nickName")
         .status(ACTIVE)
         .tradePassword("123456")
         .build();
 
     memberRepository.save(member);
+    ResignRequest resignRequest = ResignRequest.builder().password(encodedPw).build();
 
-    UserDetailsImpl userDetails = new UserDetailsImpl(member);
+    // given
+    given(memberRepository.findByEmail(member.getEmail())).willReturn(Optional.of(member));
+    given(passwordEncoder.matches(resignRequest.getPassword(), member.getPassword())).willReturn(
+        true);
+    given(redisService.getData("RT:" + member.getEmail())).willReturn("");
+    given(jwtTokenProvider.getTokenExpirationTime(member.getEmail())).willReturn(1L);
 
     // when
-    memberService.resign(member.getEmail(), userDetails, ResignRequest.builder().password(originalPw).build());
+    UserDetailsImpl userDetails = new UserDetailsImpl(member);
+    memberService.resign(fakeMemberEmail, userDetails, resignRequest);
 
     // then
-    Optional<Member> byEmail = memberRepository.findByEmail(member.getEmail());
-    if(byEmail.isEmpty()){
-      throw new MemberException(ErrorCode.USER_NOT_FOUND);
-    }
+    Member resignedMember = memberRepository.findByEmail(member.getEmail())
+        .orElseThrow(() -> new MemberException(USER_NOT_FOUND));
 
-    Member savedMember = byEmail.get();
-    assertEquals(RESIGN, savedMember.getStatus());
+    assertEquals(RESIGN, resignedMember.getStatus());
   }
+
 
   @Test
   @DisplayName("비밀번호 찾기 이메일 생성 테스트")
