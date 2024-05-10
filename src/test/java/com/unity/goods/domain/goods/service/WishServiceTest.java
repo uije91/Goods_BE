7f569b7f718a759d3573182873eb9e4d35501de5
Seong Ui-je <uije91@gmail.com>
2024-05-10 16:52:03 +0900

    Test: Test 코드 수정

--- src/test/java/com/unity/goods/domain/goods/service/WishServiceTest.java
+++ src/test/java/com/unity/goods/domain/goods/service/WishServiceTest.java
@@ -7,13 +7,15 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.unity.goods.domain.goods.dto.GoodsStatus;
 import com.unity.goods.domain.goods.dto.WishlistDto;
 import com.unity.goods.domain.goods.entity.Goods;
+import com.unity.goods.domain.goods.entity.Image;
 import com.unity.goods.domain.goods.entity.Wishlist;
 import com.unity.goods.domain.goods.exception.GoodsException;
 import com.unity.goods.domain.goods.repository.GoodsRepository;
+import com.unity.goods.domain.goods.repository.ImageRepository;
 import com.unity.goods.domain.goods.repository.WishRepository;
+import com.unity.goods.domain.goods.type.GoodsStatus;
 import com.unity.goods.domain.member.entity.Member;
 import com.unity.goods.domain.member.repository.MemberRepository;
 import com.unity.goods.domain.member.type.Role;
@@ -22,8 +24,8 @@ import com.unity.goods.domain.model.BaseEntity;
 import com.unity.goods.global.exception.ErrorCode;
 import java.lang.reflect.Field;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -32,6 +34,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @ExtendWith(MockitoExtension.class)
 class WishServiceTest {
@@ -48,7 +53,11 @@ class WishServiceTest {
   @Mock
   private WishRepository wishRepository;
 
+  @Mock
+  private ImageRepository imageRepository;
+
   private Member member;
+  private Member member2;
   private Goods goods;
 
   @BeforeEach
@@ -61,6 +70,16 @@ class WishServiceTest {
         .status(Status.ACTIVE)
         .role(Role.USER)
         .phoneNumber("010-1111-1111")
+        .build();
+
+    member2 = Member.builder()
+        .id(11L)
+        .email("test@test.com")
+        .password("test1234")
+        .nickname("test2")
+        .status(Status.ACTIVE)
+        .role(Role.USER)
+        .phoneNumber("010-2222-2222")
         .build();
 
     goods = Goods.builder()
@@ -76,26 +95,12 @@ class WishServiceTest {
   }
 
   @Test
-  @DisplayName("위시리스트 조회 실패 - 위시리스트가 존재하지 않음")
-  void getWishlist_NotFound() {
-    //given
-    when(wishRepository.findByMemberId(member.getId())).thenReturn(new ArrayList<>());
-
-    //then
-    GoodsException exception = assertThrows(GoodsException.class,
-        () -> wishService.getWishlist(member.getId()));
-
-    assertEquals(ErrorCode.WISHLIST_NOT_FOUND, exception.getErrorCode());
-  }
-
-  @Test
   @DisplayName("위시리스트 조회 성공")
   void getWishlist_success() throws NoSuchFieldException, IllegalAccessException {
     //given
     Goods goods1 = Goods.builder()
         .id(1L)
         .goodsName("테스트상품1")
-        .thumbnailImageUrl("test_url1")
         .price(20000L)
         .address("테스트 주소1")
         .goodsStatus(GoodsStatus.SALE)
@@ -105,12 +110,13 @@ class WishServiceTest {
     Goods goods2 = Goods.builder()
         .id(2L)
         .goodsName("테스트상품2")
-        .thumbnailImageUrl("test_url2")
         .price(30000L)
         .address("테스트 주소2")
         .goodsStatus(GoodsStatus.RESERVATION)
         .member(member)
         .build();
+
+    Image image = Image.builder().id(1L).imageUrl("test_url").goods(goods1).build();
 
     // BaseEntity 시간 강제 설정
     LocalDateTime goods1Time = LocalDateTime.of(2024, 4, 9, 23, 30);
@@ -128,18 +134,20 @@ class WishServiceTest {
     when(wishRepository.findByMemberId(member.getId())).thenReturn(wishlist);
     when(goodsRepository.findById(goods1.getId())).thenReturn(Optional.of(goods1));
     when(goodsRepository.findById(goods2.getId())).thenReturn(Optional.of(goods2));
+    when((imageRepository.findByGoodsId(goods1.getId()))).thenReturn(List.of(image));
+    Pageable pageable = PageRequest.of(0, 10); // 페이지 정보
 
     //when
-    List<WishlistDto> result = wishService.getWishlist(member.getId());
+    Page<WishlistDto> result = wishService.getWishlist(member.getId(),pageable);
 
     // then
-    assertEquals(2, result.size());
-    WishlistDto dto = result.get(0);
+    assertEquals(2, result.getContent().size());
+    WishlistDto dto = result.getContent().get(0);
     assertEquals("테스트상품1", dto.getGoodsName());
-    assertEquals("test_url1", dto.getThumbnailImageUrl());
     assertEquals("테스트 주소1", dto.getAddress());
     assertEquals(20000L, dto.getPrice());
     assertEquals("test", dto.getSellerName());
+    assertEquals("test_url",dto.getImageUrl());
     assertEquals(GoodsStatus.SALE, dto.getGoodsStatus());
   }
 
@@ -171,16 +179,41 @@ class WishServiceTest {
   }
 
   @Test
-  @DisplayName("위시리스트 등록 실패 - 이미 등록된 위시리스트")
-  void addWishList_AlreadyInWishlist() {
+  @DisplayName("위시리스트 등록 실패 - 내가 등록한 상품은 찜 불가능")
+  void addWishList_ImpossibleToWishlistMyGoods() {
     //given
     when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
     when(goodsRepository.findById(goods.getId())).thenReturn(Optional.of(goods));
-    when(wishRepository.existsByGoodsAndMember(goods, member)).thenReturn(true);
 
     //then
     GoodsException goodsException = assertThrows(GoodsException.class,
         () -> wishService.addWishlist(goods.getId(), member.getId()));
+
+    assertEquals(ErrorCode.IMPOSSIBLE_TO_WISHLIST_MY_GOODS, goodsException.getErrorCode());
+  }
+
+  @Test
+  @DisplayName("위시리스트 등록 실패 - 이미 등록된 위시리스트")
+  void addWishList_AlreadyInWishlist() {
+    //given
+    Member member2 = Member.builder()
+        .id(11L)
+        .email("test@test.com")
+        .password("test1234")
+        .nickname("test")
+        .status(Status.ACTIVE)
+        .role(Role.USER)
+        .phoneNumber("010-1111-1111")
+        .build();
+
+
+    when(memberRepository.findById(member2.getId())).thenReturn(Optional.of(member2));
+    when(goodsRepository.findById(goods.getId())).thenReturn(Optional.of(goods));
+    when(wishRepository.existsByGoodsAndMember(goods, member2)).thenReturn(true);
+
+    //then
+    GoodsException goodsException = assertThrows(GoodsException.class,
+        () -> wishService.addWishlist(goods.getId(), member2.getId()));
 
     assertEquals(ErrorCode.GOODS_ALREADY_WISHLIST, goodsException.getErrorCode());
   }
@@ -189,17 +222,17 @@ class WishServiceTest {
   @DisplayName("위시리스트 등록 성공")
   void addWishlist_success() {
     //given
-    when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
+    when(memberRepository.findById(member2.getId())).thenReturn(Optional.of(member2));
     when(goodsRepository.findById(goods.getId())).thenReturn(Optional.of(goods));
-    when(wishRepository.existsByGoodsAndMember(goods, member)).thenReturn(false);
+    when(wishRepository.existsByGoodsAndMember(goods, member2)).thenReturn(false);
 
     //when
-    wishService.addWishlist(goods.getId(), member.getId());
+    wishService.addWishlist(goods.getId(), member2.getId());
 
     //then
-    verify(memberRepository, times(1)).findById(member.getId());
+    verify(memberRepository, times(1)).findById(member2.getId());
     verify(goodsRepository, times(1)).findById(goods.getId());
-    verify(wishRepository, times(1)).existsByGoodsAndMember(goods, member);
+    verify(wishRepository, times(1)).existsByGoodsAndMember(goods, member2);
     verify(wishRepository, times(1)).save(any(Wishlist.class));
   }
 
