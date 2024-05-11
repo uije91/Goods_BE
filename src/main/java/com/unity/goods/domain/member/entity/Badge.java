90daa3e0cafced2296fb7db0a3fff4a226b610a8
Seong Ui-je <uije91@gmail.com>
2024-05-11 14:58:27 +0900

    Feat: 배지 조회 구현

---
+++ src/main/java/com/unity/goods/domain/member/entity/Badge.java
@@ -0,0 +1,34 @@
+package com.unity.goods.domain.member.entity;
+
+import com.unity.goods.domain.member.type.BadgeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Badge {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @Enumerated(EnumType.STRING)
+  private BadgeType badge;
+}
