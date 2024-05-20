60078e9c9517599e15b1585297e417e408b32e1b
seowonn <sonseowon5@gmail.com>
2024-05-20 21:55:30 +0900

    Test : 구매자 별점 반영 및 최종 별점 산출 기능 테스트

--- src/main/java/com/unity/goods/domain/trade/dto/StarRateDto.java
+++ src/main/java/com/unity/goods/domain/trade/dto/StarRateDto.java
@@ -1,12 +1,14 @@
 package com.unity.goods.domain.trade.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public class StarRateDto {
 
   @Getter
+  @Builder
   @NoArgsConstructor
   @AllArgsConstructor
   public static class StarRateRequest {
