aa23a06ccd8383a96c336155274940213c373d50
Seong Ui-je <uije91@gmail.com>
2024-05-24 15:22:55 +0900

    Refactor: 프론트 oauth test

--- src/main/java/com/unity/goods/domain/oauth/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/unity/goods/domain/oauth/handler/OAuth2SuccessHandler.java
@@ -1,12 +1,9 @@
 package com.unity.goods.domain.oauth.handler;
 
-import static com.unity.goods.domain.oauth.repository.CustomAuthorizationRequestRepository.REFRESH_TOKEN;
-
 import com.unity.goods.domain.model.TokenDto;
 import com.unity.goods.domain.oauth.repository.CustomAuthorizationRequestRepository;
 import com.unity.goods.global.jwt.JwtTokenProvider;
 import com.unity.goods.global.jwt.UserDetailsImpl;
-import com.unity.goods.global.util.CookieUtil;
 import com.unity.goods.infra.service.RedisService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
