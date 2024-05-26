f7284c60f72f09c30f35a2b1b9b7479e3782cefe
Seong Ui-je <uije91@gmail.com>
2024-05-27 04:11:52 +0900

    Refactor: 채팅방 목록조회, 내용확인 수정

--- src/main/java/com/unity/goods/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/unity/goods/domain/chat/repository/ChatRoomRepository.java
@@ -17,4 +17,8 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
   @Query("SELECT CASE WHEN c.sellerId = :memberId THEN c.buyerId WHEN c.buyerId = :memberId THEN c.sellerId ELSE null END " +
       "FROM ChatRoom c WHERE (c.sellerId = :memberId OR c.buyerId = :memberId) AND c.id IS NOT NULL AND c.sellerId IS NOT NULL AND c.buyerId IS NOT NULL")
   List<Long> findOppositeMemberIdByMemberId(Long memberId);
+
+  @Query("SELECT CASE WHEN c.buyerId = :memberId THEN c.sellerId WHEN c.sellerId = :memberId THEN c.buyerId ELSE NULL END " +
+      "FROM ChatRoom c WHERE c.id = :chatRoomId AND (:memberId = c.buyerId OR :memberId = c.sellerId)")
+  Long findOppositeMemberId(Long chatRoomId, Long memberId);
 }
