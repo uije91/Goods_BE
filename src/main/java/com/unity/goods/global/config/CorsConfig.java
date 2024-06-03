eb341a6a32d60a1b26258352ffee0ab603d81eb2
Seong Ui-je <uije91@gmail.com>
2024-06-03 16:34:38 +0900

    Fix: Cors 접속불가 수정

--- src/main/java/com/unity/goods/global/config/CorsConfig.java
+++ src/main/java/com/unity/goods/global/config/CorsConfig.java
@@ -11,7 +11,6 @@ public class CorsConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .exposedHeaders("Set-Cookie")
-        .allowedOrigins("http://localhost:5173")
-        .allowedOrigins("https://goods-trade.vercel.app");
+        .allowedOrigins("https://goods-trade.vercel.app", "http://localhost:5173");
   }
 }
