f8a8c1f0da865a302c1fac0591d0ce2cd7442598
seowonn <sonseowon5@gmail.com>
2024-06-10 06:23:31 +0900

    Feat: NotificationLog 테이블 생성

---
+++ src/main/java/com/unity/goods/domain/notification/entity/NotificationLog.java
@@ -0,0 +1,49 @@
+package com.unity.goods.domain.notification.entity;
+
+import com.unity.goods.domain.member.entity.Member;
+import com.unity.goods.domain.notification.type.NotificationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class NotificationLog {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "notification_log_id")
+  private long id;
+
+  @Column(nullable = false)
+  private long receiverId;
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private NotificationType notificationType;
+
+  @CreatedDate
+  @Column(updatable = false, nullable = false)
+  private LocalDateTime createdAt;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+}
