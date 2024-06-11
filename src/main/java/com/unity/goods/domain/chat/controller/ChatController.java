2d8faecf4dd7311d67ae73e802d1b46a5a0d0bc9
Seong Ui-je <uije91@gmail.com>
2024-06-11 16:48:04 +0900

    Refactor: ChatroomList, ChatLogs 페이징으로 변경

--- src/main/java/com/unity/goods/domain/chat/controller/ChatController.java
+++ src/main/java/com/unity/goods/domain/chat/controller/ChatController.java
@@ -10,6 +10,8 @@ import com.unity.goods.global.jwt.JwtTokenProvider;
 import com.unity.goods.global.jwt.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // TODO 채팅 수신 알림 기능 구현
@@ -59,14 +62,20 @@ public class ChatController {
 
   @GetMapping("/room")
   public ResponseEntity<?> getChatRoomList(
-      @AuthenticationPrincipal UserDetailsImpl principal) {
-    return ResponseEntity.ok().body(chatService.getChatRoomList(principal.getId()));
+      @AuthenticationPrincipal UserDetailsImpl principal,
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int size) {
+    Pageable pageable = PageRequest.of(page, size);
+    return ResponseEntity.ok(chatService.getChatRoomList(principal.getId(), pageable));
   }
 
   @GetMapping("/{roomId}")
   public ResponseEntity<ChatRoomDto> getChatLogs(@PathVariable Long roomId,
-      @AuthenticationPrincipal UserDetailsImpl principal) {
-    return ResponseEntity.ok().body(chatService.getChatLogs(roomId, principal.getId()));
+      @AuthenticationPrincipal UserDetailsImpl principal,
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int size) {
+    Pageable pageable = PageRequest.of(page,size);
+    return ResponseEntity.ok(chatService.getChatLogs(roomId, principal.getId(),pageable));
   }
 
   @PostMapping("room/leave/{roomId}")
